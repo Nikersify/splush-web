165dfbd817508f50b81fc96cc2be6778061a88d4
nikersify <nikersifyyt@gmail.com>
2017-04-02 17:34:59 +0200

    progress!

--- static/splush.js
+++ static/splush.js
@@ -1,14 +1,56 @@
 'use strict'
 
+// utils
+window.fetchJSON = function (url, opts) {
+	return new Promise(function (resolve, reject) {
+		fetch(url, opts).then(function (res) {
+			res.json().then(function (body) {
+				if (!res.ok) reject(body)
+				else resolve(body)
+			})
+		})
+	})
+}
+
 // constructor
 var Splush = function () {}
 
 // static methods
 Splush.isSupported = function () {
-	return !window.Notification || !window.ServiceWorker || !window.Promise
+	return !window.Notification
+		|| !window.ServiceWorker
+		|| !window.Promise
+		|| !window.fetch
 }
 
 // instance methods
+Splush.prototype.fetchKey = function () {
+	if (this.storage.exists('key')) {
+		var key = this.storage.get('key')
+		this._key = key
+		return Promise.resolve(key)
+	}
+	else
+		var self = this
+		return this.getMessagingToken().then(function (token) {
+			var headers = new Headers()
+			headers.append('Content-Type', 'application/x-www-form-urlencoded')
+
+			return fetchJSON('/sub', {
+				body: 'target=' + token,
+				headers: headers,
+				method: 'POST'
+			})
+		}).then(function (r) {
+			var key = r.res.code
+			self.storage.set('key', key)
+			self._key = key
+			return Promise.resolve(key)
+		}).catch(function (e) {
+			alert(e)
+		})
+}
+
 Splush.prototype.getMessagingToken = function () {
 	if (this._messagingToken) return new Promise.resolve(this._messagingToken)
 	else return this._messaging.getToken()
@@ -21,25 +63,42 @@ Splush.prototype.initializeMessaging = function () {
 
 	var messaging = this._messaging = firebase.messaging()
 
+	var self = this
 	messaging.onMessage(function (e) {
-		console.log('msg', e)
+		new window.Notification(e.notification.body)
 	})
 
 	messaging.onTokenRefresh(function () {
-		self._messagingToken = a
+		self.getMessagingToken().then(function (token) {
+			self._messagingToken = token
+		})
 	})
+}
 
-	return messaging.requestPermission()
+Splush.prototype.storage = {
+	exists: function (item) {
+		return !!window.localStorage.getItem(item)
+	},
+	delete: window.localStorage.removeItem.bind(window.localStorage),
+	get: window.localStorage.getItem.bind(window.localStorage),
+	flush: window.localStorage.clear.bind(window.localStorage),
+	set: window.localStorage.setItem.bind(window.localStorage)
 }
 
 // main function
 function main () {
 	var splush = window.splush = new Splush()
+	splush.initializeMessaging()
 
-	splush.initializeMessaging().then(function () {
-		return splush.getMessagingToken()
-	}).then(function (token) {
-		console.info('token', token)
+	new Promise(function (resolve, reject) {
+		try {
+			riot.compile(function () {
+				splush.tags = riot.mount('*')
+				resolve()
+			})
+		} catch (e) { reject(e) }
+	}).catch(function (e) {
+		alert(e)
 	})
 }
 
